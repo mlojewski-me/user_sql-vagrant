1bfb6412a448bb383a970c9ef075853bdeb619a0
Marcin Łojewski <marcin.lojewski@mlojewski.me>
2020-04-13 13:10:15 +0200

    UID, GID columns

--- init.sql
+++ init.sql
@@ -2,7 +2,8 @@ USE nextcloud;
 
 CREATE TABLE sql_user
 (
-  username       VARCHAR(16) PRIMARY KEY,
+  uid            INT         PRIMARY KEY AUTO_INCREMENT,
+  username       VARCHAR(16) NOT NULL UNIQUE,
   display_name   TEXT        NULL,
   email          TEXT        NULL,
   quota          TEXT        NULL,
@@ -16,20 +17,20 @@ CREATE TABLE sql_user
 
 CREATE TABLE sql_group
 (
-  name         VARCHAR(16) PRIMARY KEY,
-  display_name TEXT        NULL,
-  admin        BOOLEAN     NOT NULL DEFAULT FALSE
+  gid   INT         PRIMARY KEY AUTO_INCREMENT,
+  name  VARCHAR(16) NOT NULL UNIQUE,
+  admin BOOLEAN     NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE sql_user_group
 (
-  username   VARCHAR(16) NOT NULL,
-  group_name VARCHAR(16) NOT NULL,
-  PRIMARY KEY (username, group_name),
-  FOREIGN KEY (username) REFERENCES sql_user (username),
-  FOREIGN KEY (group_name) REFERENCES sql_group (name),
-  INDEX user_group_username_idx (username),
-  INDEX user_group_group_name_idx (group_name)
+  uid INT NOT NULL,
+  gid INT NOT NULL,
+  PRIMARY KEY (uid, gid),
+  FOREIGN KEY (uid) REFERENCES sql_user (uid),
+  FOREIGN KEY (gid) REFERENCES sql_group (gid),
+  INDEX user_group_username_idx (uid),
+  INDEX user_group_group_name_idx (gid)
 );
 
 DROP PROCEDURE IF EXISTS load_sql_users;
@@ -39,29 +40,29 @@ CREATE PROCEDURE load_sql_users()
   BEGIN
     DECLARE v_users INT UNSIGNED DEFAULT 30;
     DECLARE v_groups INT UNSIGNED DEFAULT 3;
-    DECLARE v_counter INT UNSIGNED DEFAULT 0;
+    DECLARE v_counter INT UNSIGNED DEFAULT 1;
 
     START TRANSACTION;
 
-    WHILE v_counter < v_groups DO
-      INSERT INTO sql_group (name, display_name) VALUES (
-        CONCAT('group', v_counter), CONCAT('Group ', v_counter)
+    WHILE v_counter <= v_groups DO
+      INSERT INTO sql_group (name) VALUES (
+        CONCAT('Group ', v_counter)
       );
       SET v_counter = v_counter + 1;
     END WHILE;
 
-    SET v_counter = 0;
+    SET v_counter = 1;
 
-    WHILE v_counter < v_users DO
+    WHILE v_counter <= v_users DO
       INSERT INTO sql_user (username, display_name, password, email) VALUES (
         CONCAT('user', v_counter), CONCAT('User ', v_counter), CONCAT('user', v_counter),
         CONCAT('user', v_counter, '@nextcloud')
       );
-      INSERT INTO sql_user_group (group_name, username) VALUES (
-        CONCAT('group', v_counter % v_groups), CONCAT('user', v_counter)
+      INSERT INTO sql_user_group (gid, uid) VALUES (
+        CONCAT(v_counter % v_groups + 1), v_counter
       );
-      INSERT INTO sql_user_group (group_name, username) VALUES (
-        CONCAT('group', (v_counter + 1) % v_groups), CONCAT('user', v_counter)
+      INSERT INTO sql_user_group (gid, uid) VALUES (
+        CONCAT((v_counter + 1) % v_groups + 1), v_counter
       );
       SET v_counter = v_counter + 1;
     END WHILE;
